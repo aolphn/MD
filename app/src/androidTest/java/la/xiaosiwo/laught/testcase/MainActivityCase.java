44a7371bebc2dd1d44b0bedae519fae2b1889cc9
zkh <zhangkehu@akey.me>
2015-07-01 23:58:40 +0800

    项目引入测试用例

---
+++ app/src/androidTest/java/la/xiaosiwo/laught/testcase/MainActivityCase.java
@@ -0,0 +1,51 @@
+package la.xiaosiwo.laught.testcase;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import la.xiaosiwo.laught.R;
+import la.xiaosiwo.laught.activities.MainActivity;
+
+/**
+ * Created by OF on 201-07-01 22:14.
+ */
+public class MainActivityCase extends ActivityInstrumentationTestCase2<MainActivity> {
+    private String TAG = "TestCase";
+    private Solo solo;
+    public MainActivityCase(){
+        super(MainActivity.class);
+    }
+    @Override
+    public void setUp() throws Exception {
+        // setUp() is run before a test case is started.
+        // This is where the solo object is created.
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+    @Override
+    public void tearDown() throws Exception {
+        // tearDown() is run after a test case has finished.
+        // finishOpenedActivities() will finish all the activities that have
+        // been opened during the test execution.
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
+    public void testMainActivity() throws Exception {
+        solo.clickOnButton("赞成");
+
+        solo.clickOnActionBarItem(R.id.action_personal);
+
+        solo.clickOnView(solo.getText("文字"));
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+        solo.clickOnButton("赞成");
+
+    }
+}
